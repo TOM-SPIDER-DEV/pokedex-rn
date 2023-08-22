a6a9267aab0f74731e04e0899e3966a0f9118771
Tomas Santa Cruz <tomasesantacruz@gmail.com>
2023-08-22 17:19:40 -0300

    Change: add images in readme.md

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
-import { Text } from "react-native";
 
 export default function App() {
   return (
